9c9be6e10e73c597d5f5cc0775659616053ab2a8
AlexWolff90 <A.Leon.Wolff@gmail.com>
2015-04-14 13:59:20 -0500

    Finish static pages

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_discoursi_session'
+Rails.application.config.session_store :cookie_store, key: '_discorsi_session'
