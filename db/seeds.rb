3dd2a6176a699692e020acd44849e3e20d764ace
AlexWolff90 <A.Leon.Wolff@gmail.com>
2015-04-27 10:29:02 -0500

    Add account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,9 @@ User.create!(first_name: "Example",
 						 email: "user@example.com",
 						 password: "password",
 						 password_confirmation: "password",
-						 admin: true)
+						 admin: true,
+						 activated: true,
+						 activated_at: Time.zone.now)
 
 99.times do |n|
 	first_name = Faker::Name.first_name
@@ -22,5 +24,7 @@ User.create!(first_name: "Example",
 							 last_name: last_name,
 							 email: email,
 							 password: password,
-							 password_confirmation: password)
+							 password_confirmation: password,
+							 activated: true,
+							 activated_at: Time.zone.now)
 end
