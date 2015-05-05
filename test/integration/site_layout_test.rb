304a942c54328f3bab34760489e5371ad7554365
AlexWolff90 <A.Leon.Wolff@gmail.com>
2015-05-05 15:22:28 -0500

    Finish point-counterpoint interface

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -14,6 +14,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 		assert_select "a[href=?]", about_path
 		assert_select "a[href=?]", contact_path
 		assert_select "a[href=?]", login_path
+		assert_select "a[href=?]", new_point_path, count: 0
 		assert_select "a[href=?]", users_path, count: 0
 		assert_select "a[href=?]", user_path(@user), count: 0
 		assert_select "a[href=?]", edit_user_path(@user), count: 0
@@ -21,6 +22,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 		log_in_as(@user)
 		get root_path
 		assert_select "a[href=?]", root_path, count: 2
+		assert_select "a[href=?]", new_point_path
 		assert_select "a[href=?]", help_path
 		assert_select "a[href=?]", about_path
 		assert_select "a[href=?]", contact_path
